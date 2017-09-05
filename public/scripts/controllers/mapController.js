51d20fd48c3538c6189e4c742d677788d27d37d5
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-05 13:04:10 -0700

    routing is working

--- public/scripts/controllers/mapController.js
+++ public/scripts/controllers/mapController.js
@@ -1,15 +1,16 @@
 'use strict';
-// var app = app || {};
-//
-// (function(module) {
-//   const mapController = {};
-//
-//   mapController.init = () => {
-        // this will show a dom element with an id of #about and hide all of its siblings (other dom elements in the same parent element)
-//     $('#map').show().siblings().hide();
+var app = app || {};
 
-//     app.Permits.grabData();
-//   };
-//
-//   module.mapController = mapController;
-// })(app);
+(function(module) {
+  const mapController = {};
+
+  mapController.init = () => {
+        // this will show a dom element with an id of #about and hide all of its siblings (other dom elements in the same parent element)
+    $('#map').show().siblings().hide();
+    console.log('map controller runs');
+
+    // app.Permits.grabData();
+  };
+
+  module.mapController = mapController;
+})(app);
