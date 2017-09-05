81ba0e1903a7570e4fc8ca5906825cc34fd139a2
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-05 16:07:29 -0700

    rendering pop ups to each permit location on map

--- public/scripts/views/mapView.js
+++ public/scripts/views/mapView.js
@@ -169,7 +169,31 @@ function initMap() {
     ],
             {name: 'Styled Map'});
 
+    var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 200
+  });
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.618288, lng: -122.351767},
@@ -186,14 +210,42 @@ function initMap() {
 
 }
 
+
 function loadMarkers(data) {
+  console.log(data);
+
+
+
   for (var i = 0; i < data.length; i++) {
+
+    var content =
+    `<p>${data[i].permit_type}</p>`+
+    `<p>${data[i].application_date}</p>`+
+    `<p>${data[i].description}</p>`+
+    `<p>$${data[i].value}</p>`+
+    `<p>${data[i].address}</p>`;
+
+    // add a new info window for each marker
+    var infowindow = new google.maps.InfoWindow({
+      content: content,
+      maxWidth: 200
+    });
+
     var coords = data[i].location.coordinates;
     var latLng = new google.maps.LatLng(coords[1],coords[0]);
     var marker = new google.maps.Marker({
       position: latLng,
-      icon: '../../favicons/favicon-32x32.png',
+      icon: '../../img/jackhammer2.png',
       map: map
-    });
+    })
+    // add info window event listener to each marker
+    google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
+            return function() {
+               infowindow.setContent(content);
+               infowindow.open(map,marker);
+            };
+        })(marker,content,infowindow));
+
+
   }
 }
