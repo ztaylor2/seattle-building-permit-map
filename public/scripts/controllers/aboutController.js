8a66f259ef05d69941b9862876badf0f45e86ce2
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-05 11:47:56 -0700

    added routing scafolding

---
+++ public/scripts/controllers/aboutController.js
@@ -0,0 +1,13 @@
+'use strict';
+// var app = app || {};
+//
+// (function(module) {
+//   const aboutController = {};
+//
+//   aboutController.init = () => {
+        // this will show a dom element with an id of #about and hide all of its siblings (other dom elements in the same parent element)
+//     $('#about').show().siblings().hide();
+//   };
+//
+//   module.aboutController = aboutController;
+// })(app);
