a3ce6c2f8b73ed710bbbc24b655ff8bda2504d51
Trey64 <thomasellison.r@gmail.com>
2017-09-05 09:31:43 -0700

    added open graph meta tags and adjusted google map zoom

--- public/scripts/models/permits.js
+++ public/scripts/models/permits.js
@@ -7,8 +7,8 @@ var map;
 function initMap() {
   // Create a map object and specify the DOM element for display.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.71318096, lng: -122.27698486},
-    zoom: 8
+    center: {lat: 47.618288, lng: -122.351767},
+    zoom: 12
   });
 }
 
