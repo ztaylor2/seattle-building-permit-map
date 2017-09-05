8817f8f62c7553688b6443151b387662ed3a791d
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-05 16:13:03 -0700

    map has colors, custom icons, and pop ups

--- public/scripts/views/mapView.js
+++ public/scripts/views/mapView.js
@@ -169,32 +169,6 @@ function initMap() {
     ],
             {name: 'Styled Map'});
 
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 200
-  });
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.618288, lng: -122.351767},
     zoom: 12,
@@ -213,8 +187,6 @@ function initMap() {
 
 function loadMarkers(data) {
   console.log(data);
-
-
 
   for (var i = 0; i < data.length; i++) {
 
@@ -238,6 +210,7 @@ function loadMarkers(data) {
       icon: '../../img/jackhammer2.png',
       map: map
     })
+
     // add info window event listener to each marker
     google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
             return function() {
@@ -245,7 +218,6 @@ function loadMarkers(data) {
                infowindow.open(map,marker);
             };
         })(marker,content,infowindow));
-
 
   }
 }
