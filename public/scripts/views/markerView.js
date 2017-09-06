09719a9b32ca31f87f666308961335c84d8ac3a5
Zach Taylor <ztaylor2@zagmail.gonzaga.edu>
2017-09-06 14:47:45 -0700

    added value filter

--- public/scripts/views/markerView.js
+++ public/scripts/views/markerView.js
@@ -1,10 +1,23 @@
 'use strict';
 
-$('.dropdown-content').on('click', 'a', function() {
+// categories filter
+$('#marker_categories').on('click', 'a', function() {
   for (let i = 0; i < markers.length; i++) {
     marker = markers[i];
-    // If is same type or type not picked
     if (marker.type === $(this).text()) {
+      marker.setVisible(true);
+    }
+    else {
+      marker.setVisible(false);
+    }
+  }
+})
+
+$('#marker_values').on('click', 'a', function(e) {
+  console.log(e.target.id);
+  for (let i = 0; i < markers.length; i++) {
+    marker = markers[i];
+    if (marker.value > parseInt(e.target.id)) {
       marker.setVisible(true);
     }
     // Categories don't match
