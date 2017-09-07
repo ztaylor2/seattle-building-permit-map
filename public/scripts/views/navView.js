3c4514a412edebcfc8791f35475104f143a62379
Gregory Dukes <kcils360@gmail.com>
2017-09-07 13:43:44 -0700

    erasing old code

--- public/scripts/views/navView.js
+++ public/scripts/views/navView.js
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
-$('.menu').show();
-
-$('#hamburger').click(function(){
-  $('.menu').slideToggle();
-});
-$('.dropdown').click(function(){
-  console.log(this.id);
-  $(this.id).slideToggle()
-})
-=======
 'use strict';
- // With help of Stack Overflow
+
 $(document).ready(function(){
   var menu = $('.menu');
   $('#hamburger').on('click', function(){
@@ -23,4 +12,3 @@ $(document).ready(function(){
     }
   });
 });
->>>>>>> bca9c5641057577f7be2aa079737864d0dad62ec
