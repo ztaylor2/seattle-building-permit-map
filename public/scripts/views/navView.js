70bb83b3fa6f8d25faf6c32cd00cf9d094f5f97b
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-09-07 11:05:55 -0700

    fixed nav bar hidden bug

--- public/scripts/views/navView.js
+++ public/scripts/views/navView.js
@@ -1,5 +1,14 @@
-$('.menu').show();
-
-  $('#hamburger').click(function(){
-    $('.menu').slideToggle();
+'use strict';
+ // With help of Stack Overflow
+$(document).ready(function(){
+  var menu = $('.menu');
+  $('#hamburger').on('click', function(){
+    menu.slideToggle();
   });
+  $(window).resize(function(){
+    var width = $(window).width();
+    if (width > 799 && menu.is(':hidden')){
+      menu.removeAttr('style');
+    }
+  });
+});
